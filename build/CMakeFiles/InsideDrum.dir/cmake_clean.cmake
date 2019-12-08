74bdcd9b11a9578460195215f563fcf83098b7dc
capox95 <capox95@gmail.com>
2019-12-08 21:56:27 +0100

    fix

---
+++ build/CMakeFiles/InsideDrum.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InsideDrum.dir/src/inside_drum.cpp.o"
+  "CMakeFiles/InsideDrum.dir/src/entropy.cpp.o"
+  "CMakeFiles/InsideDrum.dir/src/pointpose.cpp.o"
+  "CMakeFiles/InsideDrum.dir/src/segmentation.cpp.o"
+  "InsideDrum.pdb"
+  "InsideDrum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InsideDrum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
